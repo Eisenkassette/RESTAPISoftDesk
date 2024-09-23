9bd4633fb538d42deadc3268a244ad85e2f1c8e9
Eisenkassette <daniel.larionov@protonmail.com>
2024-09-23 18:05:13 +0200

    uncommented JWT settings

--- RESTAPISoftDesk/settings.py
+++ RESTAPISoftDesk/settings.py
@@ -45,9 +45,9 @@ INSTALLED_APPS = [
 
 # Rest Framework settings
 REST_FRAMEWORK = {
-    #'DEFAULT_AUTHENTICATION_CLASSES': [
-   #     'rest_framework_simplejwt.authentication.JWTAuthentication',
-   # ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
