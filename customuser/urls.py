7f7db0dfd651428b333883cf095d743ae5d49885
Eisenkassette <daniel.larionov@protonmail.com>
2024-09-25 06:03:18 +0200

    added user anonymize url

--- customuser/urls.py
+++ customuser/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CustomUserListCreate, CustomUserRetrieveUpdateDestroy
+from .views import CustomUserListCreate, CustomUserRetrieveUpdateDestroy, anonymize_user
 
 urlpatterns = [
     path('users/', CustomUserListCreate.as_view(), name='user-list-create'),
     path('users/<str:username>/', CustomUserRetrieveUpdateDestroy.as_view(), name='user-retrieve-update-delete'),
+
+    path('users/anonymize/<int:user_id>/', anonymize_user, name='anonymize-user'),
 ]
