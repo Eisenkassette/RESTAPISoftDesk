0c30e07293f0fd84d24fc5af67bf610a98c8fa21
Eisenkassette <daniel.larionov@protonmail.com>
2024-09-25 05:47:37 +0200

    Removed useless permission

--- projects/permissions.py
+++ projects/permissions.py
@@ -1,16 +1,5 @@
 from rest_framework import permissions
-from django.shortcuts import get_object_or_404
 from .models import Contributor
-
-
-class IsContributor(permissions.BasePermission):
-    """
-    Custom permission to only allow contributors of a project
-    to create or retrieve issues.
-    """
-    def has_permission(self, request, view):
-        project_id = view.kwargs.get('project_id')
-        return Contributor.objects.filter(user=request.user, project_id=project_id).exists()
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
@@ -27,3 +16,20 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
             return True
         return obj.author == request.user
 
+class IsAuthorOrContributorReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow:
+    - Contributors to read the object
+    - Author to modify or delete the object
+    """
+
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+        project_id = view.kwargs.get('project_id')
+        return Contributor.objects.filter(user=request.user, project_id=project_id).exists()
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
