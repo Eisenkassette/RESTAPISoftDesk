0fb106cbd03c19dabaf2d4490558884c592a8110
Eisenkassette <daniel.larionov@protonmail.com>
2024-09-25 06:03:32 +0200

    Set default permission to IsAdminUser

--- RESTAPISoftDesk/settings.py
+++ RESTAPISoftDesk/settings.py
@@ -50,7 +50,7 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAdminUser',
     ],
     
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
