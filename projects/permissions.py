f45b2cb98c8ffc0e1294dd9159e489ddbdb1a652
Daniel <157501916+Eisenkassette@users.noreply.github.com>
2024-09-25 07:42:47 +0200

    Fixed a logic issue

--- projects/permissions.py
+++ projects/permissions.py
@@ -9,7 +9,7 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     """
     
     def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS or request.user.is_authenticated
+        return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
